f43958c138ba12d7cc6fbd86b594b1072613dbc1
hapybay <hapybay@yahoo.com>
2017-07-04 14:01:32 -0700

    ignore intlj files edited .gitin

---
+++ student/src/test/java/edu/pdx/cs410J/jl/StudentTest.java
@@ -0,0 +1,44 @@
+package edu.pdx.cs410J.jl;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * Unit tests for the Student class.  In addition to the JUnit annotations,
+ * they also make use of the <a href="http://hamcrest.org/JavaHamcrest/">hamcrest</a>
+ * matchers for more readable assertion statements.
+ */
+public class StudentTest
+{
+
+  @Test
+  public void studentNamedPatIsNamedPat() {
+    // Tests should be TheGiven, TheWhen and TheThen.
+    //  Given a student named Pat
+    String name = "Pat";
+
+    // When we new a Student
+    Student pat = new Student(name, new ArrayList(), 0.0, "Doesn't matter");
+
+    // then we expect that pat's getname will give us Pat
+    assertThat(pat.getName(), equalTo(name));
+  }
+
+  @Test
+  public void studentNamedPatIsNamedPatDummy() {
+    // Tests should be TheGiven, TheWhen and TheThen.
+    //  Given a student named Pat
+    String name = "Pat";
+
+    // When we new a Student
+    Student pat = new Student(name, new ArrayList(), 0.0, "Doesn't matter");
+
+    // then we expect that pat's getname will give us Pat
+    assertThat(pat.getName(), equalTo(name));
+  }
+
+}
